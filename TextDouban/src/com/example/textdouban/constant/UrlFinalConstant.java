b327cb7d05d2916772e13a9a01eca338eab202c1
郭鹏超 <gpc_guopengchao@163.com>
2014-07-30 10:56:17 +0800

    fffff

--- TextDouban/src/com/example/textdouban/constant/UrlFinalConstant.java
+++ TextDouban/src/com/example/textdouban/constant/UrlFinalConstant.java
@@ -2,10 +2,5 @@ package com.example.textdouban.constant;
 
 public class UrlFinalConstant {
 
-	private static final String category_url = "http://en.gamept.cn/game_english/yx_category_count.php";//分类接口
-	private static final String gameDtailUrl = "http://en.gamept.cn/game_english/detail.php?";//游戏详情接口
-	private static final String relevanGameUrl = "http://en.gamept.cn/game_english/yx_lanmu_recommend.php?";//相关游戏接口
-	private static final String categoyDetailNewUrl = "http://en.gamept.cn/game_english/yx_lanmu_new.php";//最新游戏接口
-	private static final String categoyDetailHotUrl = "http://en.gamept.cn/game_english/yx_lanmu_hot.php";//最热游戏接口
 
 }
